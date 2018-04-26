6f6e6b84a64897668f6ae9a4e5bd67b861a6fa88
rossini-andrea <rossini.andrea.lc@gmail.com>
2018-04-26 11:00:56 +0200

    Fixed wrong header

--- whoneeds/Program.cs
+++ whoneeds/Program.cs
@@ -1,6 +1,6 @@
 /*
-    Mandelbrot.exe - A Mandelbrot Set generator.
-    Copyright (C) 2017 Andrea Rossini
+    whoneeds.exe - A Mandelbrot Set generator.
+    Copyright (C) 2018 Andrea Rossini
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
